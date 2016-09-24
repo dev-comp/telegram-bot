4556e5d5bff781a98e4be4ce3bb1340bc6ca08d8
dev-comp <p.v.shapoval@gmail.com>
2016-09-24 09:33:24 +0300

    Полный рефакторинг

---
+++ src/main/java/com/bftcom/devcomp/adapters/TelegramAdapter.java
@@ -0,0 +1,93 @@
+package com.bftcom.devcomp.adapters;
+
+import com.bftcom.devcomp.api.AbstractMessengerAdapter;
+import com.bftcom.devcomp.api.Configuration;
+import com.bftcom.devcomp.api.IBot;
+import com.bftcom.devcomp.api.IBotConst;
+import com.bftcom.devcomp.bots.TelegramBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.bots.BotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.BotSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ikka
+ * @date: 16.09.2016.
+ */
+public class TelegramAdapter extends AbstractMessengerAdapter<BotSession> {
+  @SuppressWarnings("PackageAccessibility")
+  private static final Logger logger = LoggerFactory.getLogger(TelegramAdapter.class);
+
+  /**
+   * Имя прослушиваемой очереди для сообщений от экземпляров ботов
+   */
+  private static final String ADAPTER_NAME = "telegram-adapter";
+
+  private final TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+
+  @Override
+  protected boolean startBotSession(String id, IBot bot) {
+    try {
+      logger.info("registering bot " + id);
+      BotSession botSession = telegramBotsApi.registerBot((TelegramLongPollingBot) bot);
+      synchronized (botSessions) {
+        botSessions.put(id, botSession);
+      }
+    } catch (TelegramApiException e) {
+      logger.error("", e);
+    }
+    return true;
+  }
+
+  @Override
+  protected IBot createNewBot(Map<String, String> serviceProps, Map<String, String> userProps) {
+    BotOptions botOptions = getBotOptionsWithProxyConfig(userProps);
+    return new TelegramBot(botOptions, serviceProps.get(IBotConst.PROP_BOT_NAME), userProps.get(Configuration.BOT_USERNAME), userProps.get(Configuration.BOT_TOKEN));
+  }
+
+  private BotOptions getBotOptionsWithProxyConfig(Map<String, String> config) {
+    BotOptions botOptions = new BotOptions();
+    String proxyHost = config.get(Configuration.PROXY_HOST);
+    String proxyPort = config.get(Configuration.PROXY_PORT);
+
+    //set proxy options only if both proxyHost and poxyPort are defined
+    if (proxyHost != null && !proxyHost.isEmpty() && proxyPort != null && Integer.parseInt(proxyPort) > 0) {
+      botOptions.setProxyHost(proxyHost);
+      botOptions.setProxyPort(Integer.parseInt(proxyPort));
+    }
+    return botOptions;
+  }
+
+  @Override
+  public boolean stopBotSession(BotSession botSession) {
+    synchronized (botSessions) {
+      if (botSession != null) {
+        botSession.close();
+      }
+    }
+    return true;
+  }
+
+
+  @Override
+  public String getAdapterName() {
+    return ADAPTER_NAME;
+  }
+
+  public static void main(String[] args) {
+    TelegramAdapter botManager = new TelegramAdapter();
+    HashMap<String, String> serviceProps = new HashMap<>();
+    serviceProps.put(IBotConst.PROP_BOT_NAME, "tester");
+    HashMap<String, String> userProps = new HashMap<>();
+    userProps.put(Configuration.BOT_USERNAME, "TelIkka");
+    userProps.put(Configuration.BOT_TOKEN, "298315362:AAEZMVxS0x6EEFTtEO8RJrSKQzl3LT0CziE");
+    botManager.startBotSession("test", userProps, serviceProps);
+  }
+}
---
+++ src/main/java/com/bftcom/devcomp/bots/TelegramBot.java
@@ -0,0 +1,156 @@
+package com.bftcom.devcomp.bots;
+
+import com.bftcom.devcomp.api.BotCommand;
+import com.bftcom.devcomp.api.IBot;
+import com.bftcom.devcomp.api.IBotConst;
+import com.bftcom.devcomp.queues.BotQueueConsumer;
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.BotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Бот для Telegram
+ *
+ * @author ikka
+ * @date: 10.09.2016.
+ */
+public class TelegramBot extends TelegramLongPollingBot implements IBot {
+  @SuppressWarnings("PackageAccessibility")
+  private static final Logger logger = LoggerFactory.getLogger(TelegramBot.class);
+
+  private String username;
+  private String token;
+  private Channel inChannel;
+  private Channel outChannel;//
+  private String outQueueName;
+  private String inQueueName;
+  private String name;
+
+  public TelegramBot(BotOptions botOptions, String name, String username, String token) {
+    super(botOptions);
+    this.name = name;
+    this.username = username;
+    this.token = token;
+  }
+
+  /**
+   * Handle incoming messages from IM users
+   *
+   * @param update Update received
+   */
+  @Override
+  public void onUpdateReceived(Update update) {
+    if (update.hasMessage()) {
+      logger.info("Message received: " + update);
+      Message message = update.getMessage();
+
+      if (message.hasText()) {
+        com.bftcom.devcomp.api.Message msgToForward = new com.bftcom.devcomp.api.Message();
+        msgToForward.setCommand(BotCommand.SERVICE_PROCESS_BOT_MESSAGE);
+        Map<String, String> userProperties = msgToForward.getUserProperties();
+        Map<String, String> serviceProperties = msgToForward.getServiceProperties();
+        userProperties.put(IBotConst.PROP_BODY_TEXT, message.getText());
+        serviceProperties.put(IBotConst.PROP_BOT_NAME, getName());
+        serviceProperties.put(IBotConst.PROP_USER_NAME, message.getFrom().getFirstName());
+        serviceProperties.put("chatId", String.valueOf(message.getChatId()));
+
+        handleUserIncomingData(msgToForward);
+      }
+
+      //todo tmp echo test  
+      if (message.hasText()) {
+        SendMessage sendMessageRequest = new SendMessage();
+        sendMessageRequest.setChatId(message.getChatId().toString());
+        sendMessageRequest.setText("DEBUG: " + message.getText());
+        try {
+          sendMessage(sendMessageRequest);
+        } catch (TelegramApiException e) {
+          logger.error("", e);
+        }
+      }
+    }
+  }
+
+  @Override
+  public String getBotUsername() {
+    return username;
+  }
+
+  @Override
+  public String getBotToken() {
+    return token;
+  }
+
+  public void setInChannel(Channel inChannel) {
+    this.inChannel = inChannel;
+  }
+
+  public Channel getInChannel() {
+    return inChannel;
+  }
+
+  public String getOutQueueName() {
+    return outQueueName;
+  }
+
+  public void setInQueueName(String inQueueName) {
+    this.inQueueName = inQueueName;
+  }
+
+  public String getInQueueName() {
+    return inQueueName;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Channel getOutChannel() {
+    return outChannel;
+  }
+
+  public void setOutChannel(Channel outChannel) {
+    this.outChannel = outChannel;
+    try {
+      new BotQueueConsumer(outChannel, this);
+    } catch (IOException e) {
+      logger.error("", e);
+    }
+  }
+
+  public void setOutQueueName(String outQueueName) {
+    this.outQueueName = outQueueName;
+  }
+
+  @Override
+  public void sendMessage(Object o) {
+    if (o instanceof SendMessage) {
+      try {
+        sendMessage((SendMessage) o);
+      } catch (TelegramApiException e) {
+        logger.error("", e);
+      }
+    }
+  }
+
+  @Override
+  public void handleDelivery(com.bftcom.devcomp.api.Message m) {
+    SendMessage sendMessageRequest = new SendMessage();
+    sendMessageRequest.setChatId(m.getServiceProperties().get("chatId")); //a chatId to send the message to
+    sendMessageRequest.setText(m.getUserProperties().get(IBotConst.PROP_BODY_TEXT));
+    sendMessage((Object) sendMessageRequest);
+  }
+}
